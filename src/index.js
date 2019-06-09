9f0dbcc20b1e7151241e7f7150250ff5bacd826f
Generosa Litton <glcodeworks@gmail.com>
2019-06-09 12:48:47 -0700

    alternate state without constructor

--- src/index.js
+++ src/index.js
@@ -2,22 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // alternate state without constructor
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
 
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-        // console.log("this is lat ", this.state.lat);
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
