4185046cde0d88f4bf68abed7537d7f1ce2ce80a
Generosa Litton <glcodeworks@gmail.com>
2019-06-09 13:09:28 -0700

    ternary expressions

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    //ternary: if lat > 0 then return summer otherwise return winter
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
 const SeasonDisplay = props => {
-  console.log(props.lat);
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
   return <div>Season Display {props.lat}</div>;
 };
 
